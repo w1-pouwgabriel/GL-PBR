0c01a5414adffd57ebf4c024ef61cf925eaecee5
Gabriel Pouw <gabriel@pouw.com>
2022-10-27 18:55:55 +0200

    [ADDED] Glad manages openGL functions

--- PBRenderer/PBRenderer/src/main.cpp
+++ PBRenderer/PBRenderer/src/main.cpp
@@ -1,6 +1,39 @@
+#include <glad/glad.h>
+// GLFW (include after glad)
+#include <glfw/glfw3.h>
 
+#include <iostream>
 
-int main()
+const int WIDTH = 720;
+const int HEIGHT = 480;
+
+int main() 
 {
-	return 0;
+    //GLAD manages function pointers for OpenGL so we want to 
+    //initialize GLAD before we call any OpenGL function
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //Initialize glfw
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+
+
+    return 0;
 }
